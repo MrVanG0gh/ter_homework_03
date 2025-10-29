e64e12b924bfed4e3ec11a503a0e50398bf96fd4
Vladimir Inshakov <mr_gogh@mail.ru>
2025-10-29 20:38:09 +0300

    Token

--- src/providers.tf
+++ src/providers.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "yandex" {
   # token     = var.token
-  service_account_key_file = file("~/.authorized_key.json")
+  service_account_key_file = var.token
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
   zone      = var.default_zone
