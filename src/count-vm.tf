b731b95952b4a0a71c550fe57fde4ac2496c500a
Vladimir Inshakov <mr_gogh@mail.ru>
2025-10-29 22:38:43 +0300

    Ex.2-1

--- src/count-vm.tf
+++ src/count-vm.tf
@@ -1,4 +1,4 @@
-# Ex.2
+# Ex.2-1
 data "yandex_compute_image" "ubuntu" {
   family            = var.family_name
 }
@@ -15,21 +15,23 @@ resource "yandex_compute_instance" "web" {
     memory          = var.web_memory
     core_fraction   = var.web_core_fraction
   }
+
   boot_disk {
     initialize_params {
       image_id     = data.yandex_compute_image.ubuntu.image_id
     }
   }
+
   scheduling_policy {
     preemptible    = var.preempt_on
   }
+
   network_interface {
 
     subnet_id      = yandex_vpc_subnet.develop.id
     nat            = var.nat_is_on
     security_group_ids = [yandex_vpc_security_group.example.id]
   }
-
   metadata = var.common_metadata
 
 }
