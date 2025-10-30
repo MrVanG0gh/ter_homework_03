819916c9e0378464ae33c0bd10d94efc8e36e751
Vladimir Inshakov <mr_gogh@mail.ru>
2025-10-30 03:50:34 +0300

    Ex.4

--- src/ansible.tf
+++ src/ansible.tf
@@ -1,11 +1,11 @@
 # Ex.4
 
 resource "local_file" "inventory" {
-  content = templatefile("${path.module}/hosts.tftpl", {
+  content = templatefile("${path.cwd}/hosts.tftpl", {
     webservers = yandex_compute_instance.web
     databases  = yandex_compute_instance.main_replica
     storage    = yandex_compute_instance.storage
   })
-  filename = "${abspath(path.module)}/hosts.cfg"
+  filename = "${abspath(path.cwd)}/hosts.cfg"
 }
 
